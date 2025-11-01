4ebee959363dacc0091a93388a35f87fc7966d73
Aditya-Shetty-08 <shettyaditya513@gmail.com>
2025-11-01 14:50:52 -0400

    UI Design of model

---
+++ ui-project.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv.imshow('Webcam Feed', frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
+
+
